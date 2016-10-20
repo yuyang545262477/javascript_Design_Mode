8e5e0cc1936b4e2807696a96c7f2da000715ed28
David_Yu <yuyang545262477@gmail.com>
2016-10-20 12:11:35 +0800

    完善生命周期js文件

--- lifeCycle.js
+++ lifeCycle.js
@@ -1,2 +1,26 @@
 /*生命周期案例*/
-    
+(function () {
+    'use strict';
+    var example_simple = function () {
+        var a = 1;
+        console.log(a);
+    };
+    // example_simple(); // 此函数执行完成后，变量a及被垃圾回收
+
+    var example_complicacy = function () {
+        var a = 1;
+        return function () {
+            a++;
+            console.log(a);
+        }
+    };
+
+    var f = example_complicacy();
+    f();
+    f();
+    f();
+    f();
+    f();
+
+})();    
+
