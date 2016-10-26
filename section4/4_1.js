f8b1aed6b5dae665c5cd097c3fded4da7604f5dd
David_Yu <yuyang545262477@gmail.com>
2016-10-26 21:40:43 +0800

    实现第一个单例模式

---
+++ section4/4_1.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    var singTon = function (name) {
+        this.name = name;
+        this.instance = null;
+    };
+
+    singTon.prototype.getName = function () {
+        alert(this.name);
+    };
+
+    singTon.getInstance = function (name) {
+        if (!this.instance) {
+            this.instance = new singTon(name);
+        }
+        return this.instance;
+    };
+
+
+    var a = singTon.getInstance('yu');
+    var b = singTon.getInstance('yang');
+
+    alert(a === b);
+
+})();
